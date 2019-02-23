a589e9984f26cd915b691e236aff50edd49a4879
ngttai <ngttai.tv@gmail.com>
2019-02-23 17:19:47 +0700

    First commit

---
+++ xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/.build/src/gcc-4.8.2/configure --build=x86_64-build_unknown-linux-gnu --host=x86_64-build_unknown-linux-gnu --target=xtensa-lx106-elf --prefix=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/builds/xtensa-lx106-elf --with-local-prefix=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/sysroot --disable-libmudflap --with-sysroot=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/sysroot --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG 1.20.0' --disable-__cxa_atexit --with-gmp=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-mpfr=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-mpc=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-isl=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-cloog=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-libelf=/home/nttai/esp/ESP8266_RTOS_V2.XX/crosstool-NG/.build/xtensa-lx106-elf/buildtools --enable-lto --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-nls --disable-multilib --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
